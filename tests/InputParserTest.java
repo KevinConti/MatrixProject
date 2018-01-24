fec890d8514518384d4524babdebed2652b0337b
Kevin Conti <marketing.KevinConti@gmail.com>
2018-01-24 18:38:31 -0500

    Added tests to InputParserTest

--- tests/InputParserTest.java
+++ tests/InputParserTest.java
@@ -17,4 +17,27 @@ public class InputParserTest {
         assertEquals(parserResult[0].length, LENGTH_OF_CLASS, 0);
         assertEquals(parserResult[1].length, LENGTH_OF_CLASS, 0);
     }
+
+    @Test
+    public void whenConvertFileIsCalledTwoCorrectClassesAreCreated(){
+        InputParser ip = new InputParser();
+        Vector[][] parserResult = ip.convertFile("data/data.txt");
+
+        Vector classOneVector = parserResult[0][0];
+        assertEquals(classOneVector.getX(), 2.566613444, 0);
+        assertEquals(classOneVector.getY(), 3.244925222, 0);
+
+        Vector classTwoVector = parserResult[1][0];
+        assertEquals(classTwoVector.getX(), 0.587942192, 0);
+        assertEquals(classTwoVector.getY(), -1.256260722, 0);
+
+        classOneVector = parserResult[0][49];
+        classTwoVector = parserResult[1][49];
+
+        assertEquals(classOneVector.getX(), 2.365301895, 0);
+        assertEquals(classOneVector.getY(), 3.16735623, 0);
+
+        assertEquals(classTwoVector.getX(), 0.579117114, 0);
+        assertEquals(classTwoVector.getY(), 1.508560248, 0);
+    }
 }
